8f239c5f499b642d9a914d7ceefef84c031fcc6a
Jörn Ott <joern.ott@ott-consult.de>
2024-06-05 14:28:12 +0200

    fix bootstrap location != /var/www/source

--- .github/oxid-esales/install.sh
+++ .github/oxid-esales/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 chmod a+x vendor/oxid-esales/oxideshop-ce/.github/oxid-esales/install.sh
+ln -s vendor/oxid-esales/oxideshop-ce/bootstrap.php .
 vendor/oxid-esales/oxideshop-ce/.github/oxid-esales/install.sh
